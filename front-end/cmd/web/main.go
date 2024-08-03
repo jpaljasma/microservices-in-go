7e9607b3236372a4bcbceb95fac11595fd1b8889
Jaan Paljasma <jpaljasma@gmail.com>
2024-08-02 22:44:46 -0400

    Add static serving

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -5,18 +5,85 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
+// LogReqInfo describes info about HTTP request
+type HTTPReqInfo struct {
+	// GET etc.
+	method  string
+	uri     string
+	referer string
+	ipaddr  string
+	// response code, like 200, 404
+	code int
+	// number of bytes of the response sent
+	size int64
+	// how long did it take to
+	duration  time.Duration
+	userAgent string
+}
+
+// func logRequestHandler(h http.Handler) http.Handler {
+// 	fn := func(w http.ResponseWriter, r *http.Request) {
+// 		ri := &HTTPReqInfo{
+// 			method: r.Method,
+// 			uri: r.URL.String(),
+// 			referer: r.Header.Get("Referer"),
+// 			userAgent: r.Header.Get("User-Agent"),
+// 		}
+
+// 		ri.ipaddr = requestGetRemoteAddress(r)
+
+// 		// this runs handler h and captures information about
+// 		// HTTP request
+// 		m := httpsnoop.CaptureMetrics(h, w, r)
+
+// 		ri.code = m.Code
+// 		ri.size = m.BytesWritten
+// 		ri.duration = m.Duration
+// 		logHTTPReq(ri)
+// 	}
+// 	return http.HandlerFunc(fn)
+// }
+
+// Create a request logging middleware handler called Logger
+type Logger struct {
+	handler http.Handler
+}
+
+// ServeHTTP handles the request by passing it to the real handler and logging the request details
+func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	l.handler.ServeHTTP(w, r)
+	log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
+}
+
+// NewLogger constructs a new Logger middleware handler
+func NewLogger(handlerToWrap http.Handler) *Logger {
+	return &Logger{handlerToWrap}
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	mux := http.NewServeMux()
+
+	fileHandler := http.StripPrefix("/static", http.FileServer(http.Dir("static")))
+	mux.Handle("/static/", fileHandler)
+
+	mux.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/favicon.ico")
+	})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		render(w, "test.page.gohtml")
 	})
 
+	//wrap entire mux with logger middleware
+	wrappedMux := NewLogger(mux)
+
 	fmt.Println("Starting front end service on port 80")
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		log.Panic(err)
-	}
+	log.Fatal(http.ListenAndServe(":80", wrappedMux))
 }
 
 func render(w http.ResponseWriter, t string) {
